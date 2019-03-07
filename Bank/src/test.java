8a446cce84431fdbc6fe2ddc637fced7b0ebaf60
orlando <orlandopereyra@outlook.fr>
2019-03-07 16:32:43 +0100

    Test TPX

---
+++ Bank/src/test.java
@@ -0,0 +1,14 @@
+
+public class test {
+	public static void main(String[] args) {
+		String driver = "";
+		String url = "";
+		String username = "root";
+		String password = "";
+		/*
+		 * try { //class.forName(driver); //Connection connection = driver;
+		 * 
+		 * }
+		 */
+	}
+}
